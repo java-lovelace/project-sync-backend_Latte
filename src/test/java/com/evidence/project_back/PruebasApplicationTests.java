01c875e6cdaf0570ad6d7ebaef094d9231792efe
Sebitas <areizasebas31@gmail.com>
2025-10-23 12:14:59 -0500

    update

---
+++ src/test/java/com/evidence/project_back/PruebasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evidence.project_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
